0b1957e89b090a7977c844d3e2d4e78573ebf2a8
zer0-star <zer0star.65535@gmail.com>
2018-08-19 20:33:15 +0900

    update to v0.0.3

--- arrlang.rb
+++ arrlang.rb
@@ -1,12 +1,12 @@
 require "formula"
 
-ARRLANG_VERSION = "0.0.2"
+ARRLANG_VERSION = "0.0.3"
 
 class Arrlang < Formula
   desc "The best language powered by \"ARROW\"."
   homepage "https://github.com/zer0-star/arrlang"
   url "https://github.com/zer0-star/arrlang/archive/v#{ARRLANG_VERSION}.tar.gz"
-  sha256 "eb2da1decafa3347157f31aad996b13e01490532a589eb1d03c2c3769edfc7a0"
+  sha256 "ecdfc43caebefcab1bf3c97b2c5d8e4f9f09e17171005aea7b6a0bae0d430cdd"
   head "https://github.com/zer0-star/arrlang.git", :tag => "v#{ARRLANG_VERSION}"
   version ARRLANG_VERSION
 
