55938dcb70095f8cb83586194e027c61a2664f6a
zer0-star <zer0star.65535@gmail.com>
2018-08-19 18:24:43 +0900

    fix sha256

--- arrlang.rb
+++ arrlang.rb
@@ -6,7 +6,7 @@ class Arrlang < Formula
   desc "The best language powered by \"ARROW\"."
   homepage "https://github.com/zer0-star/arrlang"
   url "https://github.com/zer0-star/arrlang/archive/v#{ARRLANG_VERSION}.tar.gz"
-  sha256 "38072881f2cd24494304bc22202b7c9f46010a95919474bfd8f14aa9d06a5b37"
+  sha256 "eb2da1decafa3347157f31aad996b13e01490532a589eb1d03c2c3769edfc7a0"
   head "https://github.com/zer0-star/arrlang.git", :tag => "v#{ARRLANG_VERSION}"
   version ARRLANG_VERSION
 
