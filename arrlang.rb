c813ca4820ad6e84b9863047fdf9981b1f4e7742
zer0-star <zer0star.65535@gmail.com>
2018-08-20 20:46:47 +0900

    fixed formats

--- arrlang.rb
+++ arrlang.rb
@@ -1,16 +1,15 @@
-require "formula"
-
-ARRLANG_VERSION = "0.0.3"
-
 class Arrlang < Formula
-  desc "The best language powered by \"ARROW\"."
+  desc "The best language powered by \"ARROW\""
   homepage "https://github.com/zer0-star/arrlang"
-  url "https://github.com/zer0-star/arrlang/archive/v#{ARRLANG_VERSION}.tar.gz"
+  url "https://github.com/zer0-star/arrlang/archive/v0.0.3.tar.gz"
   sha256 "ecdfc43caebefcab1bf3c97b2c5d8e4f9f09e17171005aea7b6a0bae0d430cdd"
-  head "https://github.com/zer0-star/arrlang.git", :tag => "v#{ARRLANG_VERSION}"
-  version ARRLANG_VERSION
+  head "https://github.com/zer0-star/arrlang.git", :tag => "v0.0.3"
 
   def install
     bin.install "bin/macosx/arrlang"
+  end
+
+  test do
+    system "bin/macosx/arrlang -h"
   end
 end
