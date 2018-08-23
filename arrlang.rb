354280ceff8be3b34b96a271aaf1bdb5cff8b21c
zer0-star <zer0star.65535@gmail.com>
2018-08-23 12:41:03 +0900

    change build system

--- arrlang.rb
+++ arrlang.rb
@@ -4,9 +4,11 @@ class Arrlang < Formula
   url "https://github.com/zer0-star/arrlang/archive/v0.1.0.tar.gz"
   sha256 "867e4a7aeddfe893653fb29c18fa17a55b163fd4b327b45565957b3166a56a48"
   head "https://github.com/zer0-star/arrlang.git", :tag => "v0.1.0"
+  depends_on "nim" => :build
 
   def install
-    bin.install "bin/macosx/arrlang"
+    system "make"
+    bin.install "bin/arrlang"
   end
 
   test do
